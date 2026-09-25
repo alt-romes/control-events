15b894f6a9ca0bf96b7af6b62e83ae3a5d8d9459
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2026-09-26 00:14:08 +0100

    nix: Implement darwinModule by extracting ExecStart from the nixosSystem
    Re-use the implementation of services.mosquitto by evaluting the
    nixosSystem, with the nixos control-events module instanced using the
    given options, and then extract the preStart and ExecStart scripts out
    of the mosquitto unit.
    
    These bash scripts will reference e.g. the mosquitto.conf configuration
    file (which was constructed in the nixos derivation), which itself will
    reference e.g. mosquitto builtin-plugins dynamic libraries and generally
    other packages.
    
    This all works out because we evaluate the nixos module using the darwin
    packages. So, the dynamic library store path actually references a
    darwin dylib, etc etc.

--- nix/control-node.nix
+++ nix/control-node.nix
@@ -1,11 +1,6 @@
-{ self, ... }:
-{
-  flake.darwinModules.control-events = { config, lib, pkgs, ... }:
-  {};
-
-  flake.nixosModules.control-events = { config, lib, pkgs, ... }:
-  {
-
+{ self, inputs, ... }:
+let
+  nodeOptions = { lib, ... }: {
     options = {
       services.control-events-node = {
         enable = lib.mkEnableOption "a control-events-node on this machine, which listens for control-events";
@@ -17,7 +12,7 @@
             to. The target node will typically aggregate events from various
             proxies, and mission centers or dashboards will query information
             from said target (though eventually falling back to this node if
-            the potentially remote target is unavailable).
+                the potentially remote target is unavailable).
             '';
         };
         listenOn = lib.mkOption {
@@ -25,12 +20,12 @@
           default = null;
           description = ''
             If events are meant to be proxied to this node from other nodes,
-            what address to bind the node on. "null" otherwise.
+               what address to bind the node on. "null" otherwise.
 
-            WARNING: use a private IP address like 10.0.0.3, within a private
-            (virtual) network. Currently, we rely on the VPN for security
-            rather than any TLS or password based authentication to the node.
-            '';
+                 WARNING: use a private IP address like 10.0.0.3, within a private
+                 (virtual) network. Currently, we rely on the VPN for security
+                 rather than any TLS or password based authentication to the node.
+                 '';
         };
         nodeId = lib.mkOption {
           type = lib.types.nullOr lib.types.str;
@@ -42,10 +37,61 @@
         };
       };
     };
+  };
+in
+{
+  flake.darwinModules.control-events = { config, lib, pkgs, ... }:
+  let
+    # Evaluate the nixos control-events module within the context of a full
+    # nixosSystem using the darwin packages (to get the right hashes wherever
+    # packages may be used in the config file).
+    # The point is to reuse the services.mosquitto logic for free.
+    nixosEvaluated = inputs.nixpkgs.lib.nixosSystem {
+      modules = [
+        self.nixosModules.control-events
+        {
+          nixpkgs.pkgs = pkgs;
+          services.control-events-node = {
+            inherit (cfg) enable proxyTo listenOn nodeId;
+          };
+        }
+      ];
+    };
+    cfg = config.services.control-events-node;
 
-    config =
-    let cfg = config.services.control-events-node;
-     in lib.mkIf cfg.enable {
+    mosquitto      = nixosEvaluated.config.services.mosquitto;
+    mosquitto_unit = nixosEvaluated.config.systemd.services.mosquitto;
+  in
+  {
+    imports = [ nodeOptions ];
+    config = lib.mkIf cfg.enable {
+      launchd.daemons.mosquitto = {
+        # Execute the exact same shell preStart and ExecStart commands as a
+        # nixos system, but the pkgs referenced will be darwin pkgs!
+        script = ''
+          export PATH=${pkgs.coreutils}/bin:$PATH # for `install`
+          mkdir -p ${mosquitto.dataDir}
+          ${mosquitto_unit.preStart}
+          exec ${mosquitto_unit.serviceConfig.ExecStart}
+        '';
+        serviceConfig = {
+          RunAtLoad = true;
+          KeepAlive = true;
+          WorkingDirectory = mosquitto.dataDir;
+          StandardOutPath = "/tmp/nix.services.mosquitto.out";
+          StandardErrorPath = "/tmp/nix.services.mosquitto.err";
+        };
+      };
+    };
+  };
+
+  flake.nixosModules.control-events = { config, lib, ... }:
+  let cfg = config.services.control-events-node; in
+  {
+
+    imports = [ nodeOptions ];
+
+    config = lib.mkIf cfg.enable {
 
       services.mosquitto = {
         enable = true;
